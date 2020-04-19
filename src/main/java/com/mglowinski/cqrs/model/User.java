4682d23121e10549c95f52334fb508ebf7742d19
mglowinski <mck00@o2.pl>
2020-04-19 18:07:27 +0200

    add user model

---
+++ src/main/java/com/mglowinski/cqrs/model/User.java
@@ -0,0 +1,21 @@
+package com.mglowinski.cqrs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
