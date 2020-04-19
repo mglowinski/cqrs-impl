02a197b5624b30d214740ad27ce9e42cfe09abfe
mglowinski <mck00@o2.pl>
2020-04-19 19:44:49 +0200

    add command registry component

---
+++ src/main/java/com/mglowinski/cqrs/command/CommandRegistry.java
@@ -0,0 +1,33 @@
+package com.mglowinski.cqrs.command;
+
+import com.mglowinski.cqrs.command.handler.CommandHandler;
+import org.springframework.core.GenericTypeResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class CommandRegistry {
+
+    private Map<Class<? extends Command>, CommandHandler<?, ?>> commandHandlerMap = new HashMap<>();
+
+    public CommandRegistry(Set<CommandHandler<?, ?>> commandHandlers) {
+        createRegistry(commandHandlers);
+    }
+
+    public CommandHandler<?, ?> getCommandHandler(Class<?> commandClass) {
+        return commandHandlerMap.get(commandClass);
+    }
+
+    private void createRegistry(Set<CommandHandler<?, ?>> commandHandlers) {
+        commandHandlers.forEach(commandHandler -> {
+            Class<?>[] generics = GenericTypeResolver.resolveTypeArguments(
+                    commandHandler.getClass(), CommandHandler.class);
+            Class<? extends Command> commandClass = (Class<? extends Command>) generics[1];
+            commandHandlerMap.put(commandClass, commandHandler);
+        });
+    }
+
+}
